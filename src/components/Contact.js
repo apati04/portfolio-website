bda998c4d0e92e3303be52ba3df83cdd35776961
Andrew Patipaksiri <apax714@gmail.com>
2017-03-13 15:05:18 -0700

    refactor div to pageheader for form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
 import React, { Components } from 'react';
-
+import { PageHeader } from 'react-bootstrap';
 class ContactForm extends Component {
   render() {
     return (
       <div>
-        Contact Me
+        <PageHeader>Contact Me</PageHeader>
         <div>form goes here</div>
       </div>
     )

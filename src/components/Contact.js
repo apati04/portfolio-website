cdf69432ac65fbeb2ee50b0223edc81ef15c0a2a
Andrew Patipaksiri <apax714@gmail.com>
2017-03-13 16:40:19 -0700

    scaffold contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { PageHeader } from 'react-bootstrap';
+import { PageHeader} from 'react-bootstrap';
 
 class ContactForm extends Component {
   render() {
     return (
       <div>
-        <PageHeader>Contact Me</PageHeader>
-        <form>form goes here</form>
+        <PageHeader className="text-center">Contact Me</PageHeader>
+        <form className="col-md-5 col-md-offset-4">
+          <div className="form-group">
+            <input className="form-control" placeholder="Insert your Name" type="text" />
+          </div>
+          <div className="form-group">
+            <input className="form-control" placeholder="Insert your Name" type="text" />
+          </div>
+          <div className="form-group">
+            <input className="form-control" placeholder="Insert your Name" type="text" />
+          </div>
+        </form>
       </div>
     )
   }
